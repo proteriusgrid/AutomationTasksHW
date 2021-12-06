1994d556bd898dfd26e1de78f70639a609077f37
Volodymyr Varkhol <proteriusgrid@gmail.com>
2021-12-06 13:30:06 +0200

    Added home tasks for lessons 8

---
+++ src/HWLesson8/EvenNumber.java
@@ -0,0 +1,35 @@
+package HWLesson8;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EvenNumber {
+
+
+    public  void checkEvenNumber() throws ArithmeticException{
+        Scanner sc = new Scanner(System.in).useLocale(Locale.ENGLISH.US);
+        System.out.print("number: ");
+        double number = sc.nextDouble();
+
+        if ((number % 1) == 0) {
+            if (isEven(number) == true) {
+                System.out.print("Even");
+            }else {
+                System.out.print("Odd");
+            }
+
+        } else  {
+            throw new ArithmeticException("The number entered is not an integer. ");
+        }
+
+    }
+
+
+    public static boolean isEven(double number)   {
+        return (number % 2 == 0);
+
+    }
+
+
+
+}
---
+++ src/HWLesson8/Fibonacci.java
@@ -0,0 +1,34 @@
+package HWLesson8;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    static void printFibonacciNumbers(long n)
+    {
+        long f1 = 0;
+        long f2 = 1;
+        long  i;
+
+        if (n < 1)
+            return;
+        System.out.print(f1 + " ");
+        for (i = 1; i < n; i++)
+        {
+            System.out.print(f2 + " ");
+            long next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
+    }
+
+
+
+    public void doFibonacciNumbers() {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("number: ");
+        long n= sc.nextLong();
+
+        printFibonacciNumbers(n);
+    }
+}
