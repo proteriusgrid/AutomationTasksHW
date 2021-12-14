6f2f33607d77661c98ed91c7584d442aa9ae6b12
Volodymyr Varkhol <proteriusgrid@gmail.com>
2021-12-14 19:37:39 +0200

    Added home tasks for lessons 10

---
+++ src/HWLesson10/Task1/Horse.java
@@ -0,0 +1,42 @@
+package HWLesson10.Task1;
+
+public class Horse extends Animal {
+    private double weight;
+
+    public Horse() {
+    }
+
+    public Horse(String food, String location) {
+        super(food, location);
+    }
+
+    public Horse(String food, String location, double weight) {
+        this(food, location);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Лошадь ест.");
+    }
+
+    @Override
+    public String makeNoise() {
+        return "Иии-ги-ги-гоо!!";
+    }
+
+    @Override
+    public String toString() {
+        return " Horse { " + " food: " + getFood() + '\'' + ", location: " + getLocation() + '\'' + '}';
+    }
+
+
+}
---
+++ src/HWLesson10/Task2/Student.java
@@ -0,0 +1,26 @@
+package HWLesson10.Task2;
+
+public class Student  {
+
+    String firstName;
+    String lastName;
+    String group;
+    double averageMark;
+
+    public Student  ( String firstName, String lastName, String group, double averageMark){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+
+    }
+    public Student(){
+
+    }
+    public  double getScholarship(){
+
+        return averageMark == 5 ? 100 : 80 ;
+    }
+
+}
