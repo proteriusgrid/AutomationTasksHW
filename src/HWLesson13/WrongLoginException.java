fe217f9ad06aed5ba36f1e992f4228d681f05619
Volodymyr Varkhol <proteriusgrid@gmail.com>
2022-01-09 19:57:37 +0200

    Added home tasks for lessons 13

---
+++ src/HWLesson13/WrongLoginException.java
@@ -0,0 +1,38 @@
+package HWLesson13;
+
+public class WrongLoginException extends Throwable {
+    //int loginConditionViolated = 0;
+    private String detail;
+
+    public WrongLoginException( int conditionViolated ) {
+        detail = printMessage(conditionViolated);
+    }
+
+    public String printMessage(int loginConditionViolated) {
+
+
+        switch (loginConditionViolated) {
+            case 1:
+                return ("Login  length should be equal or more than 20 characters");
+
+            case 2:
+                return ("Login should contain only acceptable symbols");
+
+        }
+        return ("");
+
+    }
+
+    public WrongLoginException() {
+        detail = "Incorrect login";
+    }
+
+
+    @Override
+    public String toString() {
+        return "Wrong Login Exception: " + detail;
+    }
+
+
+}
+
